8194775866ac5ca3379499ab600780c49fa9a791
Danny <handing@bu.edu>
2021-11-01 19:05:31 -0400

    new_vis1

--- visualization_function.R
+++ visualization_function.R
@@ -1,3 +1,17 @@
 func <- function(data){
   ggplot(data, aes(x=continent)) + geom_bar()
 }
+
+## From here Handing Zhang(8808636) added a new visualization function.
+#------------------------------------------------
+
+
+func_handing <- function(data){
+  ggplot(data, aes(x=continent)) +
+    geom_bar(col = "red") +
+    theme_bw()
+}
+
+func_handing(gapminder)
+
+#------------------------------------------------
